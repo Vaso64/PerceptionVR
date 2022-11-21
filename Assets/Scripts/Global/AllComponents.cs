330626a96008b203f332d6214d559f824fbc1322
Matej Vašek <m.vaso64@gmail.com>
2022-11-21 14:24:06 +0100

    feat(main): Added interface support for object tracking

--- Assets/Scripts/Global/AllComponents.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-namespace PerceptionVR.Global
-{
-    public static class ComponentTracking
-    {
-        public static ComponentTracker<Collider> allColliders = new();
-    }
-}
