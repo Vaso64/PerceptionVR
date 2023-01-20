13697bc7e4fb3ef7ef03c348b220c5fab1e61585
Matej Vašek <m.vaso64@gmail.com>
2023-01-20 19:06:45 +0100

    feat(Game): CLI arguments processing, manual XR runtime controls

---
+++ Assets/Scripts/Global/Startup.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using PerceptionVR.Debug;
+using PerceptionVR.Player;
+using UnityEngine.XR.Management;
+
+namespace PerceptionVR.Global
+{
+    public class Startup : MonoBehaviour
+    {
+        [SerializeField] private bool manualArgsEnabled;
+        [SerializeField] private string[] manualArgs;
+        
+        private void Start()
+        {
+            var args = manualArgsEnabled ? manualArgs : Environment.GetCommandLineArgs();
+            
+            Debugger.LogInfo(args.ToString());
+            
+            // Set mode (VR or KBM)
+            SetMode(args);
+        }
+        
+        private void SetMode(IEnumerable<string> args)
+        {
+            if (args.Contains("-vr"))
+            {
+                FindObjectOfType<VRPlayer>(includeInactive: true).gameObject.SetActive(true);
+                FindObjectOfType<KBMPlayer>(includeInactive: true).gameObject.SetActive(false);
+                StartCoroutine(StartXR());
+            }
+            
+            else
+            {
+                FindObjectOfType<VRPlayer>(includeInactive: true).gameObject.SetActive(false);
+                FindObjectOfType<KBMPlayer>(includeInactive: true).gameObject.SetActive(true);
+            }
+        }
+
+
+        private IEnumerator StartXR()
+        {
+            Debugger.LogInfo("Initializing XR...");
+            yield return XRGeneralSettings.Instance.Manager.InitializeLoader();
+
+            if (XRGeneralSettings.Instance.Manager.activeLoader == null)
+            {
+                Debugger.LogError("Initializing XR Failed. Check Editor or Player log for details.");
+            }
+            else
+            {
+                Debugger.LogInfo("Starting XR...");
+                XRGeneralSettings.Instance.Manager.StartSubsystems();
+            }
+        }
+    }
+}
